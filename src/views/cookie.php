42ad99c3164dc924b5097a603faad8a471eaa238
panrus <pan.russian@gmail.com>
2016-05-17 20:47:00 +0300

    Update cookie.php
    change i18n category name

--- src/views/cookie.php
+++ src/views/cookie.php
@@ -13,16 +13,16 @@ use yii\helpers\Html;
         <div class="modal-dialog  modal-sm">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title"><?php echo Yii::t($translationCategory, 'Cookie settings') ?></h4>
+                    <h4 class="modal-title"><?php echo Yii::t('cookie-message', 'Cookie settings') ?></h4>
                 </div>
                 <div class="modal-body">
-                    <p><?php echo Yii::t($translationCategory, 'We use cookies on our websites to help us offer you the best online experience. By continuing to use our website, you are agreeing to our use of cookies. Alternatively, you can manage them in your browser settings.') ?></p>
+                    <p><?php echo Yii::t('cookie-message', 'We use cookies on our websites to help us offer you the best online experience. By continuing to use our website, you are agreeing to our use of cookies. Alternatively, you can manage them in your browser settings.') ?></p>
                     <?php if(isset($moreInfoLink)):?>
-                        <?php echo Html::a(Yii::t($translationCategory, 'More information'), $moreInfoLink);?>
+                        <?php echo Html::a(Yii::t('cookie-message', 'More information'), $moreInfoLink);?>
                     <?php endif; ?>
                 </div>
                 <div class="modal-footer">
-                    <?php echo Html::button(Yii::t($translationCategory, 'Accept'), ['class' => 'btn btn-success CookieMessageOk']) ?>
+                    <?php echo Html::button(Yii::t('cookie-message', 'Accept'), ['class' => 'btn btn-success CookieMessageOk']) ?>
                 </div>
             </div>
         </div>
